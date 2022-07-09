8d2ebe9a2dac908f58523d21a71b0a88a92f56c7
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2022-07-09 19:42:21 -0300

    Removed another Console.WriteLine

--- MazeCreation.cs
+++ MazeCreation.cs
@@ -47,7 +47,6 @@ namespace WilsonAlgorithm
                             break;
                         }
                     }
-                    Console.WriteLine(notVisited.Count);
                     previousVertex = currentVertex;
                 }
                 
